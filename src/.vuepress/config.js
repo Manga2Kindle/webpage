f7ca8f40b6f572876af81a1116226ba9774f29e7
Eduardo Fernandez <yo@edufdezsoy.es>
2019-12-04 19:37:19 +0100

    added nav urls!

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -1,5 +1,11 @@
 module.exports = {
 	title: 'Manga2Kindle',
 	description: 'The easyest way to move mangas to your e-reader.',
-    dest: './public'
+	dest: './public',
+	themeConfig: {
+		nav: require('./config/nav')
+	},
+	extraWatchFiles: [
+		'.vuepress/config/nav.js'
+	]
 }
---
+++ src/.vuepress/config/nav.js
@@ -0,0 +1,5 @@
+module.exports = [
+	{ text: 'Need help?', link: '/help/'},
+	{ text: 'Google Play', link: 'https://play.google.com/store/apps/details?id=es.edufdezsoy.manga2kindle&hl=es' },
+	{ text: 'Github', link: 'https://github.com/EduFdezSoy/Manga2Kindle' }
+];
