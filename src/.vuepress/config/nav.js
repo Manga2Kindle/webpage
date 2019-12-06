b68b54ac2e47e6ad64a0f0fa395a886cf6fba7ba
Eduardo Fernandez <yo@edufdezsoy.es>
2019-12-06 18:28:26 +0100

    changed help link

--- src/.vuepress/config/nav.js
+++ src/.vuepress/config/nav.js
@@ -1,5 +1,5 @@
 module.exports = [
-	{ text: 'Need help?', link: '/help/'},
+	{ text: 'Need help?', link: '/help/guides/getting-started.md'},
 	{ text: 'Google Play', link: 'https://play.google.com/store/apps/details?id=es.edufdezsoy.manga2kindle&hl=es' },
 	{ text: 'Github', link: 'https://github.com/EduFdezSoy/Manga2Kindle' }
 ];
